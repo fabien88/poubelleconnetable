aaa6d3af01302dbe081eae7219ac456270b1e4dd
Fabien LENOIR <fabienlenoir@MacMac-6.local>
2016-05-29 21:25:48 +0200

    test comments added

--- src/test/java/com/example/poubelleconnetable/api/TrashEndpointTest.java
+++ src/test/java/com/example/poubelleconnetable/api/TrashEndpointTest.java
@@ -1,5 +1,15 @@
 package com.example.poubelleconnetable.api;
 
+import java.util.Arrays;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.unitils.reflectionassert.ReflectionAssert;
+import org.unitils.reflectionassert.ReflectionComparatorMode;
+
 import com.example.poubelleconnetable.utilities.OfyHelper;
 import com.google.api.server.spi.response.CollectionResponse;
 import com.google.api.server.spi.response.UnauthorizedException;
@@ -8,104 +18,91 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.googlecode.objectify.ObjectifyService;
 import com.googlecode.objectify.util.Closeable;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.unitils.reflectionassert.ReflectionAssert;
-import org.unitils.reflectionassert.ReflectionComparatorMode;
-
-import java.util.Arrays;
-import java.util.Date;
 
 public class TrashEndpointTest {
 
-    private static final User DEFAULT_USER = new User("test@exemple.com", "", null);
-    private static final User NULL_USER = null;
+	private static final User DEFAULT_USER = new User("test@exemple.com", "", null);
+	private static final User NULL_USER = null;
 
-    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
-            new LocalDatastoreServiceTestConfig().setNoStorage(true)
-    ).setEnvAuthDomain("localhost");
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+			new LocalDatastoreServiceTestConfig().setNoStorage(true)).setEnvAuthDomain("localhost");
 
-    private final TrashEndpoint trashEndpoint = new TrashEndpoint();
-    Closeable session;
+	private final TrashEndpoint trashEndpoint = new TrashEndpoint();
+	Closeable session;
 
-    @Before
-    public void setUp() throws Exception {
-        session = ObjectifyService.begin();
-        new OfyHelper().contextInitialized(null);
-        this.helper.setUp();
-    }
+	@Before
+	public void setUp() throws Exception {
+		session = ObjectifyService.begin();
+		new OfyHelper().contextInitialized(null);
+		this.helper.setUp();
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        session.close();
-        this.helper.tearDown();
-    }
+	@After
+	public void tearDown() throws Exception {
+		session.close();
+		this.helper.tearDown();
+	}
 
-    @Test
-    public void createTrash() throws Exception {
-        try {
-            trashEndpoint.createTrash(NULL_USER, "trash1", 4D);
-            Assert.fail("Method should check user is not null");
-        } catch (final UnauthorizedException e) {
-        }
-        ReflectionAssert.assertReflectionEquals(
-                new Trash("trash1", 4D),
-                trashEndpoint.createTrash(DEFAULT_USER, "trash1", 4D)
-        );
-    }
+	@Test
+	public void createTrash() throws Exception {
+		try {
+			trashEndpoint.createTrash(NULL_USER, "trash1", 4D);
+			Assert.fail("Method should check user is not null");
+		} catch (final UnauthorizedException e) {
+		}
+		ReflectionAssert.assertReflectionEquals(
+				new Trash("trash1", 4D),
+				trashEndpoint.createTrash(DEFAULT_USER, "trash1", 4D));
+	}
 
-    @Test
-    public void emptyTrash() throws Exception {
-        createTrash();
-        try {
-            trashEndpoint.emptyTrash(NULL_USER, "trash1");
-            Assert.fail("Method should check user is not null");
-        } catch (final UnauthorizedException e) {
-        }
-        trashEndpoint.emptyTrash(DEFAULT_USER, "trash1");
-    }
+	@Test
+	public void emptyTrash() throws Exception {
+		createTrash();
+		try {
+			trashEndpoint.emptyTrash(NULL_USER, "trash1");
+			Assert.fail("Method should check user is not null");
+		} catch (final UnauthorizedException e) {
+		}
+		trashEndpoint.emptyTrash(DEFAULT_USER, "trash1");
+	}
 
-    @Test
-    public void listTrash() throws Exception {
-        createTrash();
-        try {
-            trashEndpoint.listTrash(NULL_USER);
-            Assert.fail("Method should check user is not null");
-        } catch (final UnauthorizedException e) {
-        }
-        ReflectionAssert.assertReflectionEquals(
-                CollectionResponse.<Trash>builder().setItems(Arrays.asList(new Trash("trash1", 4D))).build(),
-                trashEndpoint.listTrash(DEFAULT_USER)
-        );
-    }
+	@Test
+	public void listTrash() throws Exception {
+		createTrash();
+		try {
+			trashEndpoint.listTrash(NULL_USER);
+			Assert.fail("Method should check user is not null");
+		} catch (final UnauthorizedException e) {
+		}
+		ReflectionAssert.assertReflectionEquals(
+				CollectionResponse.<Trash> builder().setItems(Arrays.asList(new Trash("trash1", 4D))).build(),
+				trashEndpoint.listTrash(DEFAULT_USER));
+	}
 
-    @Test
-    public void listTrashStatistics() throws Exception {
-        emptyTrash();
-        try {
-            trashEndpoint.listTrashStatistics(NULL_USER);
-            Assert.fail("Method should check user is not null");
-        } catch (final UnauthorizedException e) {
-        }
-        ReflectionAssert.assertReflectionEquals(
-                CollectionResponse.<TrashStatistics>builder().setItems(Arrays.asList(
-                        new TrashStatistics(new Trash("trash1", 4D), Arrays.asList(new Date())))).build(),
-                trashEndpoint.listTrashStatistics(DEFAULT_USER),
-                ReflectionComparatorMode.LENIENT_DATES
-        );
-    }
+	@Test
+	public void listTrashStatistics() throws Exception {
+		emptyTrash(); // calls create trash test and then empty trash test
+		try {
+			trashEndpoint.listTrashStatistics(NULL_USER);
+			Assert.fail("Method should check user is not null");
+		} catch (final UnauthorizedException e) {
+		}
+		ReflectionAssert.assertReflectionEquals(
+				CollectionResponse.<TrashStatistics> builder().setItems(Arrays.asList(
+						new TrashStatistics(new Trash("trash1", 4D), Arrays.asList(new Date())))).build(),
+				trashEndpoint.listTrashStatistics(DEFAULT_USER),
+				ReflectionComparatorMode.LENIENT_DATES);
+	}
 
-    @Test
-    public void deleteTrash() throws Exception {
-        emptyTrash();
-        try {
-            trashEndpoint.deleteTrash(NULL_USER, "trash1");
-            Assert.fail("Method should check user is not null");
-        } catch (final UnauthorizedException e) {
-        }
-        trashEndpoint.deleteTrash(DEFAULT_USER, "trash1");
-    }
+	@Test
+	public void deleteTrash() throws Exception {
+		emptyTrash();
+		try {
+			trashEndpoint.deleteTrash(NULL_USER, "trash1");
+			Assert.fail("Method should check user is not null");
+		} catch (final UnauthorizedException e) {
+		}
+		trashEndpoint.deleteTrash(DEFAULT_USER, "trash1");
+	}
 
 }
