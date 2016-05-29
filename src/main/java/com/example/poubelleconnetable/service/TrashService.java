b2a8ddbda1b9d157fd794679e6c6aa445c4bb270
Fabien LENOIR <fabienlenoir@MacMac-6.local>
2016-05-29 21:34:43 +0200

    Added Logging for trash service

--- src/main/java/com/example/poubelleconnetable/service/TrashService.java
+++ src/main/java/com/example/poubelleconnetable/service/TrashService.java
@@ -1,5 +1,12 @@
 package com.example.poubelleconnetable.service;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
 import com.example.poubelleconnetable.api.Trash;
 import com.example.poubelleconnetable.api.TrashStatistics;
 import com.example.poubelleconnetable.model.EmptiedTrashLogEntity;
@@ -9,196 +16,206 @@ import com.google.api.server.spi.response.NotFoundException;
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.Key;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static com.googlecode.objectify.ObjectifyService.ofy;
-
 /**
  * Service for managing trash.
  */
 public class TrashService {
 
-    /**
-     * Helper to create user key
-     *
-     * @param user
-     *
-     * @return
-     */
-    private Key<User> createUserKey(final User user) {
-        return Key.create(User.class, user.getEmail());
-    }
+	private static final Logger LOG = Logger.getLogger(TrashService.class.getName());
 
-    /**
-     * Helper to create trash key with parent user key
-     *
-     * @param user
-     * @param trashName
-     *
-     * @return
-     */
-    private Key<TrashEntity> createTrashKey(final User user, final String trashName) {
-        return Key.create(createUserKey(user), TrashEntity.class, trashName);
-    }
+	/**
+	 * Helper to create user key
+	 *
+	 * @param user
+	 *
+	 * @return
+	 */
+	private Key<User> createUserKey(final User user) {
+		return Key.create(User.class, user.getEmail());
+	}
 
-    /**
-     * create a trash and persist it in Datastore
-     *
-     * @param user      user
-     * @param trashName trashName
-     * @param volume    trash volume
-     *
-     * @return created trash
-     *
-     * @throws ConflictException if a same nammed trash already exists for this user
-     */
-    public Trash createTrash(final User user, final String trashName, final Double volume) throws ConflictException {
+	/**
+	 * Helper to create trash key with parent user key
+	 *
+	 * @param user
+	 * @param trashName
+	 *
+	 * @return
+	 */
+	private Key<TrashEntity> createTrashKey(final User user, final String trashName) {
+		return Key.create(createUserKey(user), TrashEntity.class, trashName);
+	}
 
-        if (trashExists(user, trashName)) {
-            throw new ConflictException("User has already a trash with that name");
-        }
+	/**
+	 * create a trash and persist it in Datastore
+	 *
+	 * @param user
+	 *            user
+	 * @param trashName
+	 *            trashName
+	 * @param volume
+	 *            trash volume
+	 *
+	 * @return created trash
+	 *
+	 * @throws ConflictException
+	 *             if a same nammed trash already exists for this user
+	 */
+	public Trash createTrash(final User user, final String trashName, final Double volume) throws ConflictException {
 
-        if (volume == null) {
-            throw new IllegalArgumentException("Volume should not be null");
-        }
-        if (trashName == null) {
-            throw new IllegalArgumentException("Thrash name should not be null");
-        }
+		if (trashExists(user, trashName)) {
+			throw new ConflictException("User has already a trash with that name");
+		}
 
-        final TrashEntity trash = new TrashEntity(
-                createUserKey(user),
-                trashName,
-                volume
-        );
+		if (volume == null) {
+			throw new IllegalArgumentException("Volume should not be null");
+		}
+		if (trashName == null) {
+			throw new IllegalArgumentException("Thrash name should not be null");
+		}
 
-        ofy().save().entity(trash).now();
+		final TrashEntity trash = new TrashEntity(
+				createUserKey(user),
+				trashName,
+				volume);
 
-        return new Trash(trash.getName(), trash.getVolume());
-    }
+		ofy().save().entity(trash).now();
 
-    /**
-     * Check if a trash with that name and user exists in datastore
-     *
-     * @param user      user
-     * @param trashName trash name
-     *
-     * @return true if it does
-     */
-    private boolean trashExists(final User user, final String trashName) {
-        return ofy().load().filterKey(createTrashKey(user, trashName)).count() != 0;
-    }
+		return new Trash(trash.getName(), trash.getVolume());
+	}
 
-    /**
-     * Log empty trash operation in a dedicated entity in datastores
-     *
-     * @param user      user
-     * @param trashName trash name
-     *
-     * @throws NotFoundException if the trash to empty doesn't exists
-     */
-    public void emptyTrash(final User user, final String trashName)
-            throws NotFoundException {
+	/**
+	 * Check if a trash with that name and user exists in datastore
+	 *
+	 * @param user
+	 *            user
+	 * @param trashName
+	 *            trash name
+	 *
+	 * @return true if it does
+	 */
+	private boolean trashExists(final User user, final String trashName) {
+		return ofy().load().filterKey(createTrashKey(user, trashName)).count() != 0;
+	}
 
-        if (!trashExists(user, trashName)) {
-            throw new NotFoundException("This trash doesn't exist");
-        }
+	/**
+	 * Log empty trash operation in a dedicated entity in datastores
+	 *
+	 * @param user
+	 *            user
+	 * @param trashName
+	 *            trash name
+	 *
+	 * @throws NotFoundException
+	 *             if the trash to empty doesn't exists
+	 */
+	public void emptyTrash(final User user, final String trashName)
+			throws NotFoundException {
 
-        final EmptiedTrashLogEntity emptiedTrashLogEntity = new EmptiedTrashLogEntity(
-                createTrashKey(user, trashName),
-                new Date()
-        );
+		if (!trashExists(user, trashName)) {
+			throw new NotFoundException("This trash doesn't exist");
+		}
 
-        ofy().save().entities(emptiedTrashLogEntity);
+		final EmptiedTrashLogEntity emptiedTrashLogEntity = new EmptiedTrashLogEntity(
+				createTrashKey(user, trashName),
+				new Date());
 
-    }
+		ofy().save().entities(emptiedTrashLogEntity);
 
-    /**
-     * List all trashed belongin to the given user
-     *
-     * @param user user
-     *
-     * @return trash list
-     */
-    public List<Trash> getTrashes(final User user) {
+	}
 
-        final List<TrashEntity> trashEntities = ofy()
-                .load()
-                .type(TrashEntity.class) // We want only trash
-                .ancestor(createUserKey(user))   // We filter entities on this user
-                .list();
+	/**
+	 * List all trashed belongin to the given user
+	 *
+	 * @param user
+	 *            user
+	 *
+	 * @return trash list
+	 */
+	public List<Trash> getTrashes(final User user) {
 
-        final List<Trash> trashes = new ArrayList<>();
+		final List<TrashEntity> trashEntities = ofy()
+				.load()
+				.type(TrashEntity.class) // We want only trash
+				.ancestor(createUserKey(user)) // We filter entities on this user
+				.list();
 
-        for (final TrashEntity entity : trashEntities) {
-            trashes.add(new Trash(entity.getName(), entity.getVolume()));
-        }
+		final List<Trash> trashes = new ArrayList<>();
 
-        return trashes;
-    }
+		LOG.info(String.format("%s trash fetched for specific user", trashEntities.size()));
 
-    /**
-     * Get statistics for a given trash
-     *
-     * @param user      user
-     * @param trashName trash
-     *
-     * @return statistics
-     */
-    protected List<Date> getTrashLogDateFor(final User user, final String trashName, final boolean naturalOrder) {
+		for (final TrashEntity entity : trashEntities) {
+			trashes.add(new Trash(entity.getName(), entity.getVolume()));
+		}
 
-        final String orderSign;
-        if (naturalOrder) {
-            orderSign = "";
-        } else {
-            orderSign = "-";
-        }
+		return trashes;
+	}
 
-        // Retrieve all stored log for a given trash
-        final List<EmptiedTrashLogEntity> list = ofy()
-                .load()
-                .type(EmptiedTrashLogEntity.class)
-                .ancestor(createTrashKey(user, trashName)) // We filter on this specific trash
-                .order(orderSign + "date")
-                .list();
+	/**
+	 * Get statistics for a given trash
+	 *
+	 * @param user
+	 *            user
+	 * @param trashName
+	 *            trash
+	 *
+	 * @return statistics
+	 */
+	protected List<Date> getTrashLogDateFor(final User user, final String trashName, final boolean naturalOrder) {
 
-        // For each emptying log, we add date to the list
-        final List<Date> emptyDates = new ArrayList<>();
-        for (final EmptiedTrashLogEntity entities : list) {
-            emptyDates.add(entities.getDate());
-        }
-        return emptyDates;
-    }
+		final String orderSign;
+		if (naturalOrder) {
+			orderSign = "";
+		} else {
+			orderSign = "-";
+		}
 
-    protected List<Date> getTrashLogDateFor(final User user, final String trashName) {
-        return getTrashLogDateFor(user, trashName, true);
-    }
+		// Retrieve all stored log for a given trash
+		final List<EmptiedTrashLogEntity> list = ofy()
+				.load()
+				.type(EmptiedTrashLogEntity.class)
+				.ancestor(createTrashKey(user, trashName)) // We filter on this specific trash
+				.order(orderSign + "date")
+				.list();
 
-    /**
-     * Retrieve for all trashes of a given user its statistics
-     *
-     * @param user
-     *
-     * @return
-     */
-    public List<TrashStatistics> getTrashStatistic(final User user) {
-        final List<TrashStatistics> result = new ArrayList<>();
-        for (final Trash trash : getTrashes(user)) {
+		LOG.info(String.format("%s trash logs fetched", list.size()));
 
-            result.add(new TrashStatistics(trash, getTrashLogDateFor(user, trash.getName())));
-        }
-        return result;
-    }
+		// For each emptying log, we add date to the list
+		final List<Date> emptyDates = new ArrayList<>();
+		for (final EmptiedTrashLogEntity entities : list) {
+			emptyDates.add(entities.getDate());
+		}
+		return emptyDates;
+	}
 
-    public void deleteTrash(final User user, final String trashName) {
-        // Delete logs
-        ofy().delete().keys(ofy().load()
-                .type(EmptiedTrashLogEntity.class)
-                .ancestor(createTrashKey(user, trashName))
-                .keys()).now();
+	protected List<Date> getTrashLogDateFor(final User user, final String trashName) {
+		return getTrashLogDateFor(user, trashName, true);
+	}
 
-        // Delete trash
-        ofy().delete().key(createTrashKey(user, trashName)).now();
-    }
+	/**
+	 * Retrieve for all trashes of a given user its statistics
+	 *
+	 * @param user
+	 *
+	 * @return
+	 */
+	public List<TrashStatistics> getTrashStatistic(final User user) {
+		final List<TrashStatistics> result = new ArrayList<>();
+		for (final Trash trash : getTrashes(user)) {
+
+			result.add(new TrashStatistics(trash, getTrashLogDateFor(user, trash.getName())));
+		}
+		return result;
+	}
+
+	public void deleteTrash(final User user, final String trashName) {
+		// Delete logs
+		ofy().delete().keys(ofy().load()
+				.type(EmptiedTrashLogEntity.class)
+				.ancestor(createTrashKey(user, trashName))
+				.keys()).now();
+
+		// Delete trash
+		ofy().delete().key(createTrashKey(user, trashName)).now();
+	}
 }
--- src/main/java/com/example/poubelleconnetable/utilities/Authentication.java
+++ src/main/java/com/example/poubelleconnetable/utilities/Authentication.java
@@ -8,22 +8,24 @@ import com.google.appengine.api.users.User;
  */
 public class Authentication {
 
-    /**
-     * Checks if user is authorized.
-     *
-     * @param user the user to be validated.
-     *
-     * @throws UnauthorizedException if user is not authorized.
-     */
-    public static void validateUser(final User user) throws UnauthorizedException {
-        // Google Cloud Endpoints sets user to a non-null object if the request came from an approved
-        // client and the request was correctly authenticated. However, if the request was not correctly
-        // authenticated, the user object will be null.
-        // Backend requires that all requests are authenticated and UnauthorizedException is thrown for
-        // requests that are not correctly authenticated.
-        if (user == null) {
-            throw new UnauthorizedException("The user is not authorized.");
-        }
+	/**
+	 * Checks if user is authorized.
+	 *
+	 * @param user
+	 *            the user to be validated.
+	 *
+	 * @throws UnauthorizedException
+	 *             if user is not authorized.
+	 */
+	public static void validateUser(final User user) throws UnauthorizedException {
+		// Google Cloud Endpoints sets user to a non-null object if the request came from an approved
+		// client and the request was correctly authenticated. However, if the request was not correctly
+		// authenticated, the user object will be null.
+		// Backend requires that all requests are authenticated and UnauthorizedException is thrown for
+		// requests that are not correctly authenticated.
+		if (user == null) {
+			throw new UnauthorizedException("The user is not authorized.");
+		}
 
-    }
+	}
 }
